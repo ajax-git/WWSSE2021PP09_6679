d07e581b7890e7a5e3a7bfa868a6e57e722c8345
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-08 12:56:47 +0100

    Dodaj pliki projektów.

---
+++ WWSSE2021PP09_6679/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+    Napisz program obliczający sumę n kolejnych liczb nieparzystych.
+*/
+
+int number, sum = 0;
+Console.Write("Wprowadź n: ");
+number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 != 0) sum += i;
+}
+Console.WriteLine("Suma liczb nieparzystych wynosi: {0}.", sum);
